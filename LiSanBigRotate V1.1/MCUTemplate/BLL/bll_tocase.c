507d080ac97ca9c42546af3e9e22bc0fd341fbca
NeNe <nene@jloli.cc>
2026-04-01 13:56:07 +0800

    Add skip big rotate self fix macro

--- LiSanBigRotate V1.1/MCUTemplate/BLL/bll_tocase.c
+++ LiSanBigRotate V1.1/MCUTemplate/BLL/bll_tocase.c
@@ -82,6 +82,8 @@ const int fastmove_speed[19]={10,30,40,60,80-5,80-5,80-5,80-5,80-5,80-5,80-5,80-
 	
 #define ROTATE_K		(843-25)
 #define ROTATE_B		(600) //625
+
+#define SKIP_SELF_FIX	(1)	//是否跳过自修正？
 	
 //static s32 final_offsets[19] = {0,-1,-1,1,1,1,1,2,5,5,8,2,-10,0,0,0,0,0,0}; //{0,10,12,15,20,20,20,25,25,20,15,15,18,18,10,10,10,10,0}
 static s32 final_offsets[19] = {0,0,0,0,0,0,0,0,0,0,0,0,-10,0,0,0,0,0,0};
@@ -141,6 +143,11 @@ CommonStateFlag_Type BLL_ToCase_Execute(ParamShadow_Type params, u8 *err)
 		}
 
 		speed_select = params.Param1 - last_target;
+#if SKIP_SELF_FIX
+		if(!has_zero_flag && speed_select == 0)
+			goto die;
+#endif		
+		
 		if(has_zero_flag == 0){
 			speed_select = params.Param1;
 		}else{
